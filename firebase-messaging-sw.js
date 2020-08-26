8265a50c46bbdee920560160b81cbbd7a801c15f
Yeshurun <itaiyesh@gmail.com>
2020-08-26 21:54:25 +0300

    1

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,37 +1,4 @@
-// importScripts("https://www.gstatic.com/firebasejs/7.19.0/firebase-analytics.js");
-// importScripts("https://www.gstatic.com/firebasejs/7.19.0/firebase-messaging.js");
-// importScripts("https://www.gstatic.com/firebasejs/7.19.0/firebase-firestore.js");
 
-// // Your web app's Firebase configuration
-// var firebaseConfig = {
-//     apiKey: "AIzaSyCZPlNpVnYwUzkENxRQ4_t0_gulkUoh19g",
-//     authDomain: "covid19-c8864.firebaseapp.com",
-//     databaseURL: "https://covid19-c8864.firebaseio.com",
-//     projectId: "covid19-c8864",
-//     storageBucket: "covid19-c8864.appspot.com",
-//     messagingSenderId: "848569204691",
-//     appId: "1:848569204691:web:11902404f5387163557926",
-//     measurementId: "G-WWPVFZSH0N"
-//     };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-
-// const messaging = firebase.messaging();
-
-// messaging.setBackgroundMessageHandler(function(payload) {
-// console.log('[firebase-messaging-sw.js] Received background message ', payload);
-// // Customize notification here
-// const notificationTitle = 'Background Message Title';
-// const notificationOptions = {
-//     body: 'Background Message body.',
-//     icon: '/firebase-logo.png'
-// };
-
-// return self.registration.showNotification(notificationTitle,
-//     notificationOptions);
-// });
-
-  
 importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
 importScripts(
     "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js",
@@ -64,15 +31,33 @@ messaging.setBackgroundMessageHandler(function(payload) {
         "[firebase-messaging-sw.js] Received background message ",
         payload,
     );
+
     // Customize notification here
-    const notificationTitle = "Background Message Title";
+    var dataFromServer = JSON.parse(payload.data.notification);
+
     const notificationOptions = {
-        body: "Background Message body.",
-        // icon: "/itwonders-web-logo.png",
+        body: dataFromServer.body,
+        icon: dataFromServer.icon,
+        image: dataFromServer.image,
+        data: { url: dataFromServer.data.url },
+
     };
 
     return self.registration.showNotification(
-        notificationTitle,
+        dataFromServer.title,
         notificationOptions,
     );
 });
+
+// // Notification click event listener
+// self.addEventListener('notificationclick', e => {
+//     // Close the notification popout
+//     e.notification.close();
+//     // Get all the Window clients
+//     e.waitUntil(clients.matchAll({ type: 'window' }).then(clientsArr => {
+//       // If a Window tab matching the targeted URL already exists, focus that;
+//       const hadWindowToFocus = clientsArr.some(windowClient => windowClient.url === e.notification.data.url ? (windowClient.focus(), true) : false);
+//       // Otherwise, open a new tab to the applicable URL and focus it.
+//       if (!hadWindowToFocus) clients.openWindow(e.notification.data.url).then(windowClient => windowClient ? windowClient.focus() : null);
+//     }));
+//   });
